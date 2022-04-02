1028b1b5b0efa42d312b64cdacbeaef6774692d1
Harry McGoldrick <harry_mcgoldrick@msn.com>
2022-04-02 19:52:39 +0100

    add player healthbar

---
+++ Assets/KeyValuePairSerialized.cs
@@ -0,0 +1,16 @@
+﻿[System.Serializable]
+public class KeyValuePairSerialized<TKey, TValue>
+{
+    public KeyValuePairSerialized()
+    {
+    }
+
+    public KeyValuePairSerialized(TKey key, TValue value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public TKey Key;
+    public TValue Value;
+}
---
+++ Assets/UIPlayerHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerHealth : MonoBehaviour
+{
+    public Color maxHealthColor;
+    public Color minHealthColor;
+    public Image healthbarFill;
+
+    private Damagable playerDamagable;
+
+    private void Start()
+    {
+        playerDamagable = PlayerUtils.Instance.GetPlayerGameObject().GetComponent<Damagable>();
+        playerDamagable.OnDamageTaken.AddListener(() => UpdateHealthDisplay(playerDamagable.currentHealth, playerDamagable.maxHealth));
+        UpdateHealthDisplay(playerDamagable.currentHealth, playerDamagable.maxHealth);
+    }
+
+    public void UpdateHealthDisplay(float currentHealth, float maxHealth)
+    {
+        float percentageFill = currentHealth / maxHealth;
+        healthbarFill.color = GetColorForFillValue(percentageFill);
+        healthbarFill.fillAmount = percentageFill;
+    }
+
+    public Color GetColorForFillValue(float fillValue)
+    {
+        return Color.Lerp(minHealthColor, maxHealthColor, fillValue);
+
+        //for (int i = 0; i < colorRanges.Length; i++)
+        //{
+        //    if (fillValue <= colorRanges[i].Key)
+        //    {
+        //        return colorRanges[i].Value;
+        //    }
+        //}
+
+        Debug.LogError("No valid color found for health UI");
+        return new Color();
+    }
+
+}
